b5d0b96adcc4919c0ab231b9fede5f4ca71d0802
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-12-04 17:51:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //TMDB API
 
 const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
-const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://6750482558774b0008ec5abe--transcendent-faloodeh-f3d20b.netlify.app';
 const API_URL = BASE_URL + '/trending/movie/day?'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
