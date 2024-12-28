fff8c5ca22fd189185dca7b287f54bb13bd3d0da
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2024-12-29 02:34:06 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -194,7 +194,14 @@ function openNav(movie) {
 
             if (site === 'YouTube') {
               embed.push(`
-                <iframe src="https://vidlink.pro/movie/${id}?player=jw&multiLang=true&autoplay=true" class="embed hide responsive-iframe" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe 
+                  src="https://vidlink.pro/movie/${id}?player=jw&multiLang=true&autoplay=true" 
+                  class="embed hide responsive-iframe" 
+                  frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                  allowfullscreen 
+                  style="pointer-events: none;"
+                ></iframe>               
                 <iframe src="https://moviesapi.club/movie/${id}" title="${name}" class="embed hide responsive-iframe" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 
                 
