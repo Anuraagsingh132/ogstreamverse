5e5e63a39d8cefc5a6010b7b9676b5f2f436f675
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-02-27 05:14:07 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 //TMDB API
 
 const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = BASE_URL + '/trending/movie/day?'+API_KEY;
+//const BASE_URL = 'https://api.themoviedb.org/3';
+const BASE_URL = 'https://tmdb-proxy-flax.vercel.app';
+//const API_URL = BASE_URL + '/trending/movie/day?'+API_KEY;
+const API_URL = BASE_URL + '/trending/movie/day?';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
