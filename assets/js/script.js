19d6df89e4f76815c3d21a24eee9d7cca1bdcf5a
Anuraag Singh <89829204+Anuraagsingh132@users.noreply.github.com>
2025-02-27 05:21:58 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,13 @@
 //TMDB API
 
-/*const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
-//const BASE_URL = 'https://api.themoviedb.org/3';
-const BASE_URL = 'https://tmdb-proxy-flax.vercel.app';
-//const API_URL = BASE_URL + '/trending/movie/day?'+API_KEY;
-const API_URL = BASE_URL + '/trending/movie/day?';
-const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;*/
 const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-const BASE_URL = 'https://tmdb-proxy-flax.vercel.app';
+const API_URL = BASE_URL + '/trending/movie/day?'+API_KEY;
 
-const API_URL = BASE_URL + '/trending/movie/day';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?query='; // Removed API_KEY since it's not needed
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
 
 const genres = [
     
